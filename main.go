50ceb02f16769c7198b9e44282b758266b67c65e
imihocevich <ivo.mihocevich@mercadolibre.com>
2022-12-23 16:28:14 -0300

    1

---
+++ main.go
@@ -0,0 +1,15 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// var nombre = "Ivo Mihocevich"
+// var direccion = "Martin Coronado 3140"
+// var temperatura = 30
+
+// func main() {
+// 	fmt.Println(nombre)
+// 	fmt.Println(direccion)
+// 	fmt.Println("La temperatura en Cordoba es:", temperatura)
+// }
